3fea0ef27651d1744ed4e272349e5b48fe7150b5
Majdawad88 <47667880+Majdawad88@users.noreply.github.com>
2025-01-13 20:33:16 -0500

    Update DCMotor.py

--- DCMotor.py
+++ DCMotor.py
@@ -1,44 +1,42 @@
+#git clone https://github.com/Majdawad88/ECET411_MotorDC.git
+
 import RPi.GPIO as GPIO
 from time import sleep
 
-# Pins for Motor Driver Inputs 
-Motor1A = 17
-Motor2A = 18
-Motor1E = 21
- 
+# Pins for Motor Driver Inputs
+
+Motor1E = 13
+Motor1A = 19
+Motor2A = 26
+
+
 def setup():
-	GPIO.setmode(GPIO.BCM)				# GPIO Numbering
-	GPIO.setup(Motor1A,GPIO.OUT)  # All pins as Outputs
-	GPIO.setup(Motor2A,GPIO.OUT)
-	GPIO.setup(Motor1E,GPIO.OUT)
- 
+        GPIO.setmode(GPIO.BCM)                          # GPIO Numbering
+        GPIO.setup(Motor1A,GPIO.OUT)  # All pins as Outputs
+        GPIO.setup(Motor2A,GPIO.OUT)
+        GPIO.setup(Motor1E,GPIO.OUT)
+
 def loop():
-	print("Going Clockwise")
-	GPIO.output(Motor1A,GPIO.HIGH)
-	GPIO.output(Motor2A,GPIO.LOW)
-	GPIO.output(Motor1E,GPIO.HIGH)
-	sleep(5)
+        print("Going Clockwise")
+        GPIO.output(Motor1A,GPIO.LOW)
+        GPIO.output(Motor2A,GPIO.HIGH)
+        GPIO.output(Motor1E,GPIO.HIGH)
+        sleep(5)
 
         print("Stop")
         GPIO.output(Motor1E,GPIO.LOW)
-	sleep(5)
+        sleep(5)
 
-	print("Going Counter Clockwise")
-	GPIO.output(Motor1A,GPIO.LOW)
-	GPIO.output(Motor2A,GPIO.HIGH)
-	GPIO.output(Motor1E,GPIO.HIGH)
- 	sleep(5)
+        print("Going Counter Clockwise")
+        GPIO.output(Motor1A,GPIO.HIGH)
+        GPIO.output(Motor2A,GPIO.LOW)
+        GPIO.output(Motor1E,GPIO.HIGH)
+        sleep(5)
 
-	print("Stop")
-	GPIO.output(Motor1E,GPIO.LOW)
-	sleep(5)
+        print("Stop")
+        GPIO.output(Motor1E,GPIO.LOW)
+        sleep(5)
 
-def destroy():	
-	GPIO.cleanup()
-
-if __name__ == '__main__':     # Program start from here
-	setup()
-	try:
-    		loop()
-  	except KeyboardInterrupt:
-		destroy()
+setup()
+while True:
+        loop()
